9f7a3a0382794cf0478084844ef129f48c0173c1
garnercochran <103946205+garnercochran@users.noreply.github.com>
2025-08-14 15:20:43 -0400

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.post('/notify', async (req, res) => {
   const id = uuidv4();
   sessions[id] = { name, status: "waiting" };
 
-  const responseLink = `https://https://berry-doorbell.onrender.com/respond?id=${id}`;
+  const responseLink = `https://berry-doorbell.onrender.com/respond?id=${id}`;
   const payload = {
     content: `🔔 ${name} rang the Berry College Doorbell! [Click here to respond](${responseLink})`
   };
