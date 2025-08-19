0f02459b0fd4a4c289f4928f2ecc93b412aaa255
garnercochran <103946205+garnercochran@users.noreply.github.com>
2025-08-19 15:46:59 -0400

    remove Lines

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
 const axios = require('axios');
 const app = express();
